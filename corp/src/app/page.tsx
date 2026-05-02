c08cd44ef502143c928a9b8a0637b0326c3ebec5
XenderMd <denis.stavilamd@gmail.com>
2026-05-02 13:57:51 +0300

    adding images in next js

--- corp/src/app/page.tsx
+++ corp/src/app/page.tsx
@@ -1,8 +1,18 @@
+import Image from 'next/image';
+import homeImg from 'public/home.jpg';
+
 export default function Home() {
   return (
     <div>
-      <h1 className='text-3xl font-bold underline'>Hello world!</h1>
-      <p className='mt-4 text-lg'>Welcome to my Next.js app.</p>
+      Home Page
+      <div className='absolute -z-10 inset-0'>
+        <Image
+          src={homeImg}
+          alt='car factory'
+          fill
+          style={{ objectFit: 'cover' }}
+        />
+      </div>
     </div>
   );
 }

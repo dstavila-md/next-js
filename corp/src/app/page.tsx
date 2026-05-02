9d2d0880fe8fcf51dc81bf4b04302d9d2a7f1f09
XenderMd <denis.stavilamd@gmail.com>
2026-05-02 13:03:02 +0300

    implementing comming ui

--- corp/src/app/page.tsx
+++ corp/src/app/page.tsx
@@ -1,15 +1,8 @@
-import Link from 'next/link';
-
 export default function Home() {
   return (
     <div>
       <h1 className='text-3xl font-bold underline'>Hello world!</h1>
       <p className='mt-4 text-lg'>Welcome to my Next.js app.</p>
-      <div>
-        <Link href='/performance'>Performance</Link>
-        <Link href='/reliability'>Reliability</Link>
-        <Link href='/scale'>Scale</Link>
-      </div>
     </div>
   );
 }

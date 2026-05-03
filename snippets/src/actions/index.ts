520b704633620617d876a63eccb342d8947be3c9
XenderMd <denis.stavilamd@gmail.com>
2026-05-03 20:26:56 +0300

    calling a server action from a client component

--- snippets/src/actions/index.ts
+++ snippets/src/actions/index.ts
@@ -1,7 +1,9 @@
 'use server';
 
 import { db } from '@/db';
+import { redirect } from 'next/navigation';
 
-export async function editSnippet() {
-  console.log('Edit snippet called');
+export async function editSnippet(id: number, code: string) {
+  await db.snippet.update({ where: { id }, data: { code } });
+  redirect(`/snippets/${id}`);
 }

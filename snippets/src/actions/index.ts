ccc6c751f49c624a502624ec75541d8dbc50b2da
XenderMd <denis.stavilamd@gmail.com>
2026-05-09 15:40:16 +0300

    implemented useActionState in the new snippet page

--- snippets/src/actions/index.ts
+++ snippets/src/actions/index.ts
@@ -12,3 +12,24 @@ export async function deleteSnippet(id: number) {
   await db.snippet.delete({ where: { id } });
   redirect('/');
 }
+
+export async function createSnippet(
+  formstate: { message: string },
+  formData: FormData,
+) {
+  // // Check the user inputs and make sure they are valid
+  // const title = formData.get('title') as string;
+  // const code = formData.get('code') as string;
+  // // Create a new record in the database
+  // const snippet = await db.snippet.create({
+  //   data: {
+  //     title,
+  //     code,
+  //   },
+  // });
+  // // Redirect the user back to the root route
+  // redirect('/');
+  return {
+    message: 'Title must be longer',
+  };
+}

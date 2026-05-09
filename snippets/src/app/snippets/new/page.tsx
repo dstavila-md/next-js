488268b8959bf46fb511258ace826af06fa544d3
XenderMd <denis.stavilamd@gmail.com>
2026-05-09 16:03:08 +0300

    adding styling

--- snippets/src/app/snippets/new/page.tsx
+++ snippets/src/app/snippets/new/page.tsx
@@ -11,6 +11,8 @@ export default function SnippetCreatePage() {
   function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
     const formData = new FormData(event.currentTarget);
+    // Start the action and pass the form data to it
+    // We use startTransition to show the pending state of the action
     startTransition(() => action(formData));
   }
 
@@ -39,7 +41,11 @@ export default function SnippetCreatePage() {
             id='code'
           />
         </div>
-        <div>{formState.message}</div>
+        {formState.message && (
+          <div className='my-2 p-2 bg-red-200 border rounded border-red-400'>
+            {formState.message}
+          </div>
+        )}
         <button type='submit' className='rounded p-2 bg-blue-200'>
           Create
         </button>

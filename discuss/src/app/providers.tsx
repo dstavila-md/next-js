5722bb61628917943536237a254b802bdad759cc
XenderMd <denis.stavilamd@gmail.com>
2026-05-10 19:19:31 +0300

    signIn, signout & checking auth status

--- discuss/src/app/providers.tsx
+++ discuss/src/app/providers.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { NextUIProvider } from '@nextui-org/react';
+import { SessionProvider } from 'next-auth/react';
 
 interface ProvidersProps {
   children: React.ReactNode;
@@ -9,5 +10,9 @@ interface ProvidersProps {
 // This component is used to wrap the entire application with the NextUIProvider, which provides the necessary context for NextUI components to work properly.
 // It should be used in the root layout of the application.
 export default function Providers({ children }: ProvidersProps) {
-  return <NextUIProvider>{children}</NextUIProvider>;
+  return (
+    <SessionProvider>
+      <NextUIProvider>{children}</NextUIProvider>
+    </SessionProvider>
+  );
 }

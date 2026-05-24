39911f70605dab2c5a3d00d1698723179af312e1
XenderMd <denis.stavilamd@gmail.com>
2026-05-24 16:58:44 +0300

    displaying validation errors

--- discuss/src/components/topics/topic-create-form.tsx
+++ discuss/src/components/topics/topic-create-form.tsx
@@ -1,3 +1,6 @@
+'use client';
+
+import { useActionState } from 'react';
 import {
   Popover,
   PopoverContent,
@@ -9,13 +12,17 @@ import {
 import * as actions from '@/actions';
 
 export default function TopicCreateForm() {
+  const [formState, action] = useActionState(actions.createTopic, {
+    errors: {},
+  });
+
   return (
     <Popover placement='left'>
       <PopoverTrigger>
         <Button color='primary'>Create a Topic</Button>
       </PopoverTrigger>
       <PopoverContent>
-        <form action={actions.createTopic}>
+        <form action={action}>
           <div className='flex flex-col gap-4 p-4 w-80'>
             <h3 className='text-lg'>Createa a Topic</h3>
             <Input
@@ -23,12 +30,16 @@ export default function TopicCreateForm() {
               label='Name'
               labelPlacement='outside'
               placeholder='Name'
+              isInvalid={!!formState.errors.name}
+              errorMessage={formState.errors.name?.join(', ')}
             />
             <Textarea
               name='description'
               label='Description'
               labelPlacement='outside'
               placeholder='Describe your topic'
+              isInvalid={!!formState.errors.description}
+              errorMessage={formState.errors.description?.join(', ')}
             />
             <Button type='submit'>Submit</Button>
           </div>
